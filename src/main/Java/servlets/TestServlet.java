79ab9e2ad7089dc7e808502baa7429555c1562e0
zy <784174329@qq.com>
2020-03-12 19:23:07 +0800

    1

---
+++ src/main/Java/servlets/TestServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import dao.TestDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //初始化Spring容器ApplicationContext，加载配置文件
+        ApplicationContext appCon = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //通过容器获取test实例
+        TestDao tt = (TestDao)appCon.getBean("test");
+        tt.sayHello();
+        response.getWriter().println("test has done!?");
+    }
+}
